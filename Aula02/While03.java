8326d7981d7eb4f111428ee3424817d64c32754b
JpBuilds2402 <joao.pedro.er.2402@gmail.com>
2026-02-27 17:32:50 -0300

    Improve guessing game feedback and input validation

--- Aula02/While03.java
+++ Aula02/While03.java
@@ -3,21 +3,30 @@ import java.util.Scanner;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
         Random gerador = new Random();
         int sorteado = gerador.nextInt(100) + 1;
         System.out.println("Resposta: "+sorteado);
         System.out.print("Tente adivinhar um valor entre 1 e 100:");
         int palpite = scanner.nextInt();
         while (palpite != sorteado){
+            double diferenca = sorteado - palpite;
             if (palpite <= 0 || palpite >= 101){
                 System.out.print("É um numero entre 1 e 100 seu idiota:");
                 palpite = scanner.nextInt();
             }
             else {
-                System.out.print("Tente outro numero:");
-                palpite = scanner.nextInt();
+                if (diferenca > 0){
+                    System.out.println("O numero é maior que o seu palpite");
+                    System.out.print("Tente outro numero:");
+                    palpite = scanner.nextInt();
+                }
+                else {
+                    System.out.println("O numero é menor que o seu palpite");
+                    System.out.print("Tente outro numero:");
+                    palpite = scanner.nextInt();
+                }
             }
         }
         System.out.println("Parabéns acertou o numero "+sorteado);
