bd89a064acf98bfd30fb8d37d0ac2c969072f96e
JpBuilds2402 <joao.pedro.er.2402@gmail.com>
2026-03-04 20:09:18 -0300

    Add PopulacaoBaratas class for cockroach population management

---
+++ Aula04/Classes02/PopulacaoBaratas.java
@@ -0,0 +1,20 @@
+package entidade1;
+import java.util.Random;
+
+public class PopulacaoBaratas {
+    public double quantidade;
+
+    public PopulacaoBaratas (){
+        Random gerador = new Random();
+        this.quantidade = gerador.nextInt(91)+10;
+    }
+    public void aumentarBaratas (double valor){
+        this.quantidade *= valor;
+    }
+    public void spray () {
+        this.quantidade *= 0.9;
+    }
+    public int getNumeroBaratas (){
+        return (int) this.quantidade;
+    }
+}
