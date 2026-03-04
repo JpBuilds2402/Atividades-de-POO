ede1f22c27e4be30c13a6f58d150d233d52894ff
JpBuilds2402 <joao.pedro.er.2402@gmail.com>
2026-03-04 20:08:33 -0300

    Add SimulacaoCozinha class for cockroach simulation

---
+++ Aula04/Classes02/SimulacaoCozinha.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+import entidade1.PopulacaoBaratas;
+
+public class SimulacaoCozinha {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        PopulacaoBaratas cozinha = new PopulacaoBaratas();
+        System.out.println("Na cozinha tem:"+cozinha.getNumeroBaratas());
+
+        cozinha.aumentarBaratas(2);
+        System.out.println("As baratas se reproduziram e agora tem 2 vezes mais baratas que antes:"+cozinha.getNumeroBaratas());
+
+        cozinha.spray();
+        System.out.println("Depois de passar spray agora tem:"+cozinha.getNumeroBaratas());
+
+        cozinha.aumentarBaratas(3);
+        System.out.println("As baratas se reproduziram e agora tem 3 vezes mais baratas depois do spray:"+cozinha.getNumeroBaratas());
+
+        scanner.close();
+    }
+}
+
+
+
+
+
